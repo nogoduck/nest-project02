1d9b2f829ef5c3c6656eebfcee190ddd01903e26
adx <nogo22@naver.com>
2022-01-15 05:47:17 +0900

    import nest-project01

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,25 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CatsModule } from './cats/cats.module';
+import { CatsService } from './cats/cats.service';
+import { LoggerMiddleware } from './common/middlewares/logger.middleware';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    CatsModule,
+    MongooseModule.forRoot(
+      'mongodb+srv://ad:1114@table0.zotlh.mongodb.net/cats?retryWrites=true&w=majoritys',
+    ),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, CatsService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
