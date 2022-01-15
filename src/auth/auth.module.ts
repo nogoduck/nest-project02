5a99f4aa8a4030ac031ce6476a178e5b4ad4e408
adx <nogo22@naver.com>
2022-01-16 00:36:28 +0900

    Swagger security settings.

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,13 +4,18 @@ import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
 import { JwtStrategy } from './jwt/jwt.strategy';
 import { CatsModule } from '../cats/cats.module';
+import { ConfigModule } from '@nestjs/config';
 
 //session: false (세션 쿠키를 사용하지 않음)
 //JwtModule: JWT 생성해주는 모듈
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     PassportModule.register({ defaultStrategy: 'jwt', session: false }),
-    JwtModule.register({ secret: 'secret', signOptions: { expiresIn: '1y' } }),
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '1y' },
+    }),
     forwardRef(() => CatsModule),
   ],
   providers: [AuthService, JwtStrategy],
